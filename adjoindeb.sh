a7f6d9bf058183735e4abee6856b47f1947eb698
Wojciech Tęczyński <132875401+W0jT3c@users.noreply.github.com>
2023-05-10 12:19:14 +0200

    Update adjoindeb.sh

--- adjoindeb.sh
+++ adjoindeb.sh
@@ -13,22 +13,25 @@ if ! sudo realm join -U $nazwa_uzytkownika_administratora@${nazwa_domeny} ${nazw
     exit 1
 fi
 t_n="t"
-  while [[ "$t_n" == "t" ]]; do
-    echo "Enter The Name Of The Group That Should Be In The Sudo Group (Name Displayed In Lowercase Letters With Spaces):"
-    read nazwa_grupy_sudo
-    if [[ -z "$nazwa_grupy_sudo" ]]; then
+while [[ "$t_n" == "t" ]]; do
+  echo "Enter The Name Of The Group That Should Be Allowed To Log In (Name Displayed In Lowercase Letters With Spaces):"
+  read -r nazwa_grupy_logowania
+  if [[ -n "$nazwa_grupy_logowania" ]]; then
+    if grep -q "simple_allow_groups" /etc/sssd/sssd.conf; then
+      sed -i "s/simple_allow_groups = \(.*\)/simple_allow_groups = \1, ${nazwa_grupy_logowania}@${nazwa_domeny}/" /etc/sssd/sssd.conf
     else
-    nazwa_grupy_sudo=${nazwa_grupy_sudo// /\\ }
-    echo "%$nazwa_grupy_sudo@$nazwa_domeny ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
-    echo "Add Another Group? (t/n)"
-    read t_n
+      echo "simple_allow_groups = ${nazwa_grupy_logowania}@${nazwa_domeny}" >> /etc/sssd/sssd.conf
     fi
-  done
+  else
+    break
+  fi
+  echo "Add Another Group? (t/n)"
+  read t_n
+done
 echo "access_provider = simple" >> /etc/sssd/sssd.conf
 sudo pam-auth-update --enable mkhomedir
-systemctl restart sssd
 if [ -d /usr/share/lightdm/lightdm.conf.d/ ]; then
-  if ! grep -q "greeter-show-manual-login=true" /usr/share/lightdm/lightdm.conf.d/50-unity-greeter.conf &> /dev/null; then
+  if ! grep -q "greeter-show-manual-login=true" /usr/share/lightdm/lightdm.conf.d/50-unity-greeter.conf > &/dev/null; then
     echo "greeter-show-manual-login=true" >> /usr/share/lightdm/lightdm.conf.d/50-unity-greeter.conf
     sed -i 'greeter-show-manual-login=false' /usr/share/lightdm/lightdm.conf.d/50-unity-greeter.conf
     if  grep -q "greeter-show-manual-login=false" /usr/share/lightdm/lightdm.conf.d/50-unity-greeter.conf &> /dev/null; then
@@ -45,19 +48,21 @@ if [ -d /etc/lightdm/lightdm.conf.d/ ]; then
   fi
 fi
 if systemctl status lightdm.service &> /dev/null; then
-   systemctl restart lightdm.service
+    systemctl restart lightdm.service
 fi
+systemctl restart sssd
 t_n="t"
-  while [[ "$t_n" == "t" ]]; do
-    echo "Enter The Name Of The Group That Should Be In The Sudo Group(Name Displayed In Lowercase Letters With Spaces):"
-    read nazwa_grupy_sudo
-    if [[ -z "$nazwa_grupy_sudo" ]]; then
-    else
+while [[ "$t_n" == "t" ]]; do
+  echo "Enter The Name Of The Group That Should Be In The Sudo Group (Name Displayed In Lowercase Letters With Spaces):"
+  read -r nazwa_grupy_sudo
+  if [[ -n "$nazwa_grupy_sudo" ]]; then
     nazwa_grupy_sudo=${nazwa_grupy_sudo// /\\ }
     echo "%$nazwa_grupy_sudo@$nazwa_domeny ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
-    echo "Add Another Group? (t/n)"
-    read t_n
-    fi
-  done
-echo "Script Finished, You Can Now Login To The Domain"
+  else
+    break
+  fi
+  echo "Add Another Group? (t/n)"
+  read t_n
+done
+echo "Script Finished."
 exit
